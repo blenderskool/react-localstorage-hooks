0201edbea4446d1b2087a30cfd46473ca94245fe
Akash Hamirwasia <akash.hamirwasia@gmail.com>
2021-06-27 12:37:42 +0530

    Add named exports

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export { default as useLocalStorageState } from './useLocalStorageState/useLocalStorageState';
+export { default as useLocalStorageSelector } from './useLocalStorageSelector/useLocalStorageSelector';
+export { default as useLocalStorageReducer, createLocalStorageDispatcher } from './useLocalStorageReducer/useLocalStorageReducer';
