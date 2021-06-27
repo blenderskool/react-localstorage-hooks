83f1195d15bff8294ffc425d81a74d6f588a7c74
Akash Hamirwasia <akash.hamirwasia@gmail.com>
2021-06-27 12:30:40 +0530

    Add serializer and deserliazer

---
+++ src/utils.ts
@@ -0,0 +1,3 @@
+export const desearlize = <T>(data: string | null): T => data === 'undefined' ? undefined : JSON.parse(data!);
+
+export const serialize = <T>(data: T): string => data === undefined ? 'undefined' : JSON.stringify(data);
